871b818c7c3fb59080d2f589b6de8178b3bfc357
liudong <liudong.edward@gmail.com>
2018-05-12 16:49:37 +0800

    Refactoring

--- Sources/Device+App.swift
+++ Sources/Device+App.swift
@@ -11,9 +11,10 @@ import Foundation
 extension Device {
     
     public static var appName: String {
-        if let name = Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"] as? String {
+        let key = "CFBundleDisplayName"
+        if let name = Bundle.main.localizedInfoDictionary?[key] as? String {
             return name
-        } else if let name = Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String {
+        } else if let name = Bundle.main.infoDictionary?[key] as? String {
             return name
         } else {
             return "UNKNOWN NAME"
@@ -21,9 +22,10 @@ extension Device {
     }
     
     public static var appVersion: String {
-        if let version = Bundle.main.localizedInfoDictionary?["CFBundleShortVersionString"] as? String {
+        let key = "CFBundleShortVersionString"
+        if let version = Bundle.main.localizedInfoDictionary?[key] as? String {
             return version
-        } else if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+        } else if let version = Bundle.main.infoDictionary?[key] as? String {
             return version
         } else {
             return "UNKNOWN VERSION"
@@ -31,9 +33,10 @@ extension Device {
     }
     
     public static var appBuild: String {
-        if let build = Bundle.main.localizedInfoDictionary?["CFBundleVersion"] as? String {
+        let key = "CFBundleVersion"
+        if let build = Bundle.main.localizedInfoDictionary?[key] as? String {
             return build
-        } else if let build = Bundle.main.localizedInfoDictionary?["CFBundleVersion"] as? String {
+        } else if let build = Bundle.main.infoDictionary?[key] as? String {
             return build
         } else {
             return "UNKNOWN BUILD"
