31c9558c0c3f29a5cc0116b21d97e8af6cb27b3b
liudong <liudong.edward@gmail.com>
2018-05-12 16:40:10 +0800

    Bug fix

--- Sources/Device+App.swift
+++ Sources/Device+App.swift
@@ -10,18 +10,33 @@ import Foundation
 
 extension Device {
     
-    public var appName: String {
-        let name = Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"] as? String
-        return name ?? "UNKNOWN NAME"
+    public static var appName: String {
+        if let name = Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"] as? String {
+            return name
+        } else if let name = Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String {
+            return name
+        } else {
+            return "UNKNOWN NAME"
+        }
     }
     
-    public var appVersion: String {
-        let version = Bundle.main.localizedInfoDictionary?["CFBundleShortVersionString"] as? String
-        return version ?? "UNKNOWN VERSION"
+    public static var appVersion: String {
+        if let version = Bundle.main.localizedInfoDictionary?["CFBundleShortVersionString"] as? String {
+            return version
+        } else if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            return version
+        } else {
+            return "UNKNOWN VERSION"
+        }
     }
     
-    public var appBuild: String {
-        let build = Bundle.main.localizedInfoDictionary?["CFBundleVersion"] as? String
-        return build ?? "UNKNOWN BUILD"
+    public static var appBuild: String {
+        if let build = Bundle.main.localizedInfoDictionary?["CFBundleVersion"] as? String {
+            return build
+        } else if let build = Bundle.main.localizedInfoDictionary?["CFBundleVersion"] as? String {
+            return build
+        } else {
+            return "UNKNOWN BUILD"
+        }
     }
 }
